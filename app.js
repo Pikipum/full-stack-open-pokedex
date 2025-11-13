606a58cc166e83f853f153f04633abf419e01872
Markus Tervonen <markus.tervonen@hotmail.com>
2025-11-13 13:52:30 +0200

    simulating error in pipeline

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('13')
+  asdres.send('13')
 })
 
 app.get('/health', (req, res) => {
